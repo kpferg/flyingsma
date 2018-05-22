8d4d5367a290bb5786e285668f2371d2883bcdbc
Kevin Ferguson <kevin.ferguson@portlandwebworks.com>
2018-05-22 09:09:13 -0500

    Fix submenu navigation (submenues not working on mobile menu)

--- themes/creative/static/js/creative.js
+++ themes/creative/static/js/creative.js
@@ -10,9 +10,11 @@
     // jQuery for page scrolling feature - requires jQuery Easing plugin
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
-        }, 1250, 'easeInOutExpo');
+        if($($anchor.attr('href')).offset()) {
+            $('html, body').stop().animate({
+                scrollTop: ($($anchor.attr('href')).offset().top - 50)
+            }, 1250, 'easeInOutExpo');
+        }
         event.preventDefault();
     });
 
@@ -23,7 +25,7 @@
     })
 
     // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function() {
+    $('.navbar-collapse ul li a:not(.dropdown-toggle)').click(function() {
         $('.navbar-toggle:visible').click();
     });
 
